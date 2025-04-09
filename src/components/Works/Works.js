d5943369e5ed7b053760bcfe05c1e6d83f101836
daniel rozenblat <danielroz12345@gmail.com>
2025-04-09 19:21:29 +0300

    newcommit

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -2,24 +2,24 @@ import styles from "./Works.module.css"
 import result1 from "../../images/בתאל צרפתי עבודות 1.png"
 import result2 from "../../images/בתאל צרפתי עבודות 2.png"
 import result3 from "../../images/בתאל צרפתי עבודות 3.png"
-import result4 from "../../images/בתאל צרפתי עבודות 4.png"
+
 import result5 from "../../images/בתאל צרפתי עבודות 5.png"
-import result6 from "../../images/בתאל צרפתי עבודות 6.png"
+
 import result7 from "../../images/בתאל צרפתי עבודות 7.png"
 import result8 from "../../images/בתאל צרפתי עבודות 8.png"
-import result9 from "../../images/בתאל צרפתי עבודות 9.png"
+
 import result10 from "../../images/בתאל צרפתי עבודות 10.png"
 import result11 from "../../images/בתאל צרפתי עבודות 11.png"
-import result12 from "../../images/בתאל צרפתי עבודות 12.png"
+
 import result13 from "../../images/בתאל צרפתי עבודות 13.png"
-import result14 from "../../images/בתאל צרפתי עבודות 14.png"
+
 
 import result16 from "../../images/בתאל צרפתי עבודות 16.png"
-import result17 from "../../images/בתאל צרפתי עבודות 17.png"
+
 import result18 from "../../images/בתאל צרפתי עבודות 18.png"
-import result19 from "../../images/בתאל צרפתי עבודות 19.png"
+
 import result20 from "../../images/בתאל צרפתי עבודות 20.png"
-import result21 from "../../images/בתאל צרפתי עבודות 21.png"
+
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -55,10 +55,7 @@ const Works=()=>{
           type: 'image',
           src: result10,
         },
-        {
-          type: 'image',
-          src: result9,
-        },
+    
         {
           type: 'image',
           src: result2,
@@ -67,51 +64,33 @@ const Works=()=>{
           type: 'image',
           src: result3,
         },
-        {
-          type: 'image',
-          src: result4,
-        }, 
+    
         {
           type: 'image',
           src: result11,
         },
-        {
-          type: 'image',
-          src: result12,
-        },
+ 
         {
           type: 'image',
           src: result13,
         },
-        {
-          type: 'image',
-          src: result14,
-        },
+
      
         {
           type: 'image',
           src: result16,
         },
-        {
-          type: 'image',
-          src: result17,
-        },
+    
         {
           type: 'image',
           src: result18,
         },
-        {
-          type: 'image',
-          src: result19,
-        },
+     
         {
           type: 'image',
           src: result5,
         },
-          {
-            type: 'image',
-            src: result6,
-          },
+    
           {
             type: 'image',
             src: result7,
--- src/screens/ThirdScreen.js
+++ src/screens/ThirdScreen.js
@@ -6,7 +6,7 @@ const ThirdScreen=()=>{
 
 return <>
    <div className={styles.title}>במהלך המסע המקצועי שלי</div>
-    <div className={styles.description}>נתקלתי בעשרות מקרים של חוסר מקצועיות שהובילו לזיהומים ותקלות אצל לקוחות שהגיעו אלי לתיקונים</div>
+    <div className={styles.description}>נתקלתי בעשרות עבודות שגויות של חוסר מקצועיות שהובילו לתקלות אצל לקוחות שהגיעו אלי לתיקונים</div>
     <div className={styles.description}>וגם בלא מעט עצמאיות שעברו קורסים ששיטת הלימוד בהם פשוט לא התאימה להן ושיתפו אותי שהן מרגישות שהן עדיין לא מוכנות </div>
     <div className={styles.title}>לכן הקמתי את</div>
     <h1 className={styles.Specialtitle}>PRO LASH ACADEMY</h1>
