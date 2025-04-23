8ee36bf0ecc72036c74e2314d545f7348826eafc
daniel rozenblat <danielroz12345@gmail.com>
2025-04-23 15:13:13 +0300

    newFixess

--- src/screens/FifthScreen.js
+++ src/screens/FifthScreen.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import styles from './FifthScreen.module.css';
 import Button from '../components/WAbutton/Button';
 import { Users, Award, Info, Plus, Minus } from 'lucide-react';
-import acne1 from "../images/טיפול אקנה.jpg"
-import acne2 from "../images/טיפול אקנה 2.jpg"
-
-const ServiceCard = ({ title, description, suitableFor, benefits, images }) => {
+import acne1 from "../images/בתאל אפילציה.png"
+import acne2 from "../videos/בתאל אקנה.mp4"
+import antiaging from "../images/בתאל צרפתי אנטי אייגינג.png"
+const ServiceCard = ({ title, description, suitableFor, benefits, image, isVideo = false }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => {
@@ -73,12 +73,25 @@ const ServiceCard = ({ title, description, suitableFor, benefits, images }) => {
             </div>
           </div>
         </div>
-        <div className={styles.imageGallery}>
-          {images.map((image, index) => (
-            <div key={index} className={styles.imageContainer}>
-              <img src={image} alt={`${title} result ${index + 1}`} className={styles.resultImage} />
-            </div>
-          ))}
+        <div className={styles.singleImageContainer}>
+          {isVideo ? (
+            <video 
+  src={image} 
+  autoplay
+  muted
+  playsInline={true}
+  controls 
+  className={styles.mediaContent}
+>
+  Your browser does not support the video tag.
+</video>
+          ) : (
+            <img 
+              src={image} 
+              alt={`${title} result`} 
+              className={styles.mediaContent} 
+            />
+          )}
         </div>
         <div className={styles.buttonContainer}>
           <Button 
@@ -98,72 +111,48 @@ const MyServices = () => {
       description: "עור חלק ובריא. את הטיפול נתאים לפי סוג ומצב העור לאחר אבחון מקצועי.",
       suitableFor: "מנערים/ות ועד מבוגרים כל מי שסובל מאקנה, רוזצאה, סבוריאה ועור בעייתי.",
       benefits: "איזון בלוטות החלב, הפחתת דלקת, שיקום והרגעת העור לתוצאה חלקה ובריאה.",
-      images: [
-        acne1,
-        acne1,
-        acne2,
-        acne2
-      ]
+      image: acne2,
+      isVideo: true
     },
     {
       title: "אפילציה",
       description: "הסרת שיער לצמיתות בטכנולוגיה מדויקת",
       suitableFor: "לכל סוגי העור והשיער, כולל שיער בהיר, פלומתי ועיקש.",
       benefits: "פתרון ממוקד וקבוע באמצעות מחט חשמלית, הרס שורש השערה למניעת צמיחה חוזרת.",
-      images: [
-        acne1,
-        acne1,
-        acne2,
-        acne2
-      ]
+      image: acne1,
+      isVideo: false
     },
     {
       title: "טיפול בצלקות",
       description: "שיקום מרקם וטקסטורה בעור. את הטיפול נתאים לסובלים מצלקות אקנה, פוסט אקנה (כתמים).",
       suitableFor: "למעוניינות בשיקום מרקם וטקסטורה בעור, טשטוש צלקות ועיבוי העור.",
       benefits: "טשטוש צלקות, שיפור מרקם וגוון העור ועידוד חידוש לעומק העור.",
-      images: [
-        acne1,
-        acne1,
-        acne2,
-        acne2
-      ]
+      image: acne2,
+      isVideo: false
     },
     {
       title: "טיפול בפיגמנטציה",
       description: "איזון וגוון אחיד. את הטיפול נתאים לאחר אבחון סוג הפיגמנטציה.",
       suitableFor: "לסובלות מכתמי שמש, גיל, הורמונים או פוסט-אקנה.",
       benefits: "הבהרת כתמים, איזון ייצור המלנין, חידוש תאים ושיפור מרקם העור למראה זוהר ואחיד.",
-      images: [
-        acne1,
-        acne1,
-        acne2,
-        acne2
-      ]
+      image: acne2,
+      isVideo: false
     },
     {
       title: "טיפול אנטי-אייג'",
       description: "מיצוק העור, טשטוש קמטוטים, חידוש ואיחוד גוון העור. הטיפול מסייע בהאטת סימני הגיל, מיצוק העור ושיפור הקמטוטים.",
       suitableFor: "לכל מי שמעוניינת לטפל בעור ולהימנע ״מזריקות״.",
       benefits: "עידוד ייצור קולגן, הגברת גמישות ולחות, שיפור קמטוטים ושיפור מרקם וגוון העור למראה רענן וזוהר.",
-      images: [
-        acne1,
-        acne1,
-        acne2,
-        acne2
-      ]
+      image:antiaging,
+      isVideo: false
     },
     {
       title: "Face yoga",
       description: "עור בריא יותר, שיפור בריאות העור באופן טבעי ללא חומרים חיצוניים",
       suitableFor: "למעוניינים בשיפור טבעי של מראה העור ושרירי הפנים.",
       benefits: "הרקמות העמוקות מתעוררות לחיים, מערכת הניקוז הלימפטי מתחילה לעבוד ביעילות, שרירי הפנים המכווצים משתחררים, זרימת הדם משתפרת, הפחתת נפיחויות ודלקות קלות, שיפור הקמטוטים ניכר לעין.",
-      images: [
-        acne1,
-        acne1,
-        acne2,
-        acne2
-      ]
+      image: acne2,
+      isVideo: false
     }
   ];
 
